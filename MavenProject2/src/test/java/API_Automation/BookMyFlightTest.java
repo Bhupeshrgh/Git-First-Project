71040ff2127b548ec198c80e23b76d0e4d8529e0
Bhupesh <Bhupesh@LAPTOP-FA8PPKJD>
2022-01-28 23:07:15 +0530

    Second Commit

--- MavenProject2/src/test/java/API_Automation/BookMyFlightTest.java
+++ MavenProject2/src/test/java/API_Automation/BookMyFlightTest.java
@@ -8,8 +8,12 @@ public class BookMyFlightTest {
 		System.out.println("Booking Tickets");
 	}
 	@Test
+	public void Confirmtickets() {
+		System.out.println("COnfirmed Tickets");
+	}
+	@Test
 	public void Printtickets() {
-		System.out.println("Printing Tickets");
+		System.out.println("Print Tickets");
 	}
 
 }
